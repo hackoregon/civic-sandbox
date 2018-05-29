bd17a463635fc84e43f722798b7381353e453a27
Ara <araoshinsky@gmail.com>
2018-05-29 16:43:12 -0700

    Update lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 def lambda_handler(event, context):
     body = {
-    'AWS lambda' : 'UPDATED 4:20pm'} 
+    'AWS lambda' : 'UPDATED 4:43pm'} 
     return {
       'statusCode': 200, 
       'body': json.dumps(body)
