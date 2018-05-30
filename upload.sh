75f1f38ce6acfb1555a0ac7cd77d8b532f7cae9b
Ara <araoshinsky@gmail.com>
2018-05-29 18:12:42 -0700

    Update lambda function name

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 zip -r code.zip lambda_function.py
-aws lambda update-function-code --function-name='Travis-Test' --zip-file=fileb://code.zip
+aws lambda update-function-code --function-name='civicSandboxPackages' --zip-file=fileb://code.zip
