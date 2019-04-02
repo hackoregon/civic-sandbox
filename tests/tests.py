b187af350f563cdf3dc68bbc12c1bce8acbb3705
Nathan Miller <nmiller217@gmail.com>
2019-04-01 22:06:06 -0700

    refactor dictionary to be global and add test cases

--- tests/tests.py
+++ tests/tests.py
@@ -289,13 +289,78 @@ body = {
     }
 
 expected_return = {
-    'statusCode': 200, 
+    'statusCode' : 200, 
     'body' : body
     }
 
+def test_returned_value_is_not_none():
+    assert lambda_function.lambda_handler(None, None) is not None
+
+def test_returned_value_is_a_dictionary():
+    assert type(lambda_function.lambda_handler(None, None)) is dict
+
 def test_returned_dictionary_value():
     returned_map = lambda_function.lambda_handler(None, None)
+    assert returned_map is not None
     assert expected_return == returned_map
+
+def test_statusCode_is_200():
+    returned_map = lambda_function.lambda_handler(None, None)
+    assert returned_map is not None
+    status_code = returned_map['statusCode']
+    assert status_code is not None
+    assert status_code == 200
+
+def test_body_has_correct_elements():
+    returned_map = lambda_function.lambda_handler(None, None)
+    assert returned_map is not None
+    body = returned_map['body']
+    assert body is not None
+    foundations = body['foundations']
+    assert foundations is not None
+    packages = body['packages']
+    assert packages is not None
+    slides = body['slides']
+    assert slides is not None
+
+def test_foundations_elements_have_correct_structure():
+    returned_map = lambda_function.lambda_handler(None, None)
+    assert returned_map is not None
+    body = returned_map['body']
+    assert body is not None
+    foundations = body['foundations']
+    assert foundations is not None
+    for k,v in foundations.items():
+        assert 'name' in v
+        assert 'endpoint' in v
+        assert 'visualization' in v
+
+def test_slides_elements_have_correct_structure():
+    returned_map = lambda_function.lambda_handler(None, None)
+    assert returned_map is not None
+    body = returned_map['body']
+    assert body is not None
+    slides = body['slides']
+    assert slides is not None
+    for k,v in slides.items():
+        assert 'name' in v
+        assert 'endpoint' in v
+        assert 'visualization' in v
+
+def test_packages_elements_have_correct_structure():
+    returned_map = lambda_function.lambda_handler(None, None)
+    assert returned_map is not None
+    body = returned_map['body']
+    assert body is not None
+    packages = body['packages']
+    assert packages is not None
+    for k,v in packages.items():       
+        assert 'description' in v
+        assert 'foundations' in v
+        assert 'slides' in v
+        assert 'default_foundation' in v
+        assert 'default_slide' in v
+        assert 'slides' in v
 
 if __name__ == "__main__":
     test_returned_dictionary_value()
