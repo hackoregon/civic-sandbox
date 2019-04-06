c2462115b0f491198f10d64681e78a5a152bf2cf
Nathan Miller <nmiller217@gmail.com>
2019-04-06 00:34:44 -0700

    more comment refactoring

--- lambda_function.py
+++ lambda_function.py
@@ -11,14 +11,17 @@ package_info_contexts = {
 # endpoint suffix for package info definitions at each backend repo
 package_info_endpoint_url_suffix = 'sandbox/package_info/'
 
-"""
- Package Definitions:  
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
   Old "un-decentralized" style of locating all package definitions, for all packages for all backend repos
   in one monolithic dictionary inside the civic-sandbox's lambda_function endpoint. This has been replaced
   by the new decentralized style of locating package defitions for each backend repo in the respective 
   backend repo's 'sandbox/package_info/' endpoint. While the lamda_function's location is still at the 
   same place, it now fetches each backend repo's package definitions from the backend repo's 
   'sandbox/package_info/' endpoint in a transparent manner.    
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+"""
+ Package Definitions 
 """
 packages = { 
   'Greenspace': {
@@ -91,7 +94,7 @@ packages = {
 
 
 """
- Slides:
+ Slides: 
  Used in formulating the package definitions
 """
 slides = {
