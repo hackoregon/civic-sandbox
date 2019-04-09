4e1a89f31230b13584d41e7bfca6f158f0f90841
Nathan Miller <nmiller217@gmail.com>
2019-04-09 15:21:03 -0700

    add exception handling for remote endpoint connection

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
-from urllib import request, parse
+from urllib import request, parse, error
 import json
 import package_info as local_package_info
 
@@ -16,13 +16,18 @@ def update_local_package_info():
     # formulate the url of the remote context's package definitions endpoint
     remote_package_info_endpoint = parse.urljoin(remote_context_url_base, remote_package_info_endpoint_suffix)
 
-    with request.urlopen(remote_package_info_endpoint) as remote_endpoint_url:
-      # fetch any packages defined at the remote context
-      remote_package_info = json.loads(remote_endpoint_url.read().decode())
-      # add any remotely-defined packages to the local copy
-      local_package_info.packages.update(remote_package_info['packages'])
-      local_package_info.foundations.update(remote_package_info['foundations'])
-      local_package_info.slides.update(remote_package_info['slides'])
+    try:
+      with request.urlopen(remote_package_info_endpoint) as remote_endpoint_url:
+        # fetch any packages defined at the remote context
+        remote_package_info = json.loads(remote_endpoint_url.read().decode())
+        # add any remotely-defined packages to the local copy
+        local_package_info.packages.update(remote_package_info['packages'])
+        local_package_info.foundations.update(remote_package_info['foundations'])
+        local_package_info.slides.update(remote_package_info['slides'])
+    except error.HTTPError as he:
+      print(he)
+    except error.URLError as ue:
+      print(ue)
 
 
 def lambda_handler(event, context):
